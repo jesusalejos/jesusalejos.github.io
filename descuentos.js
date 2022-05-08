74935d1d52515f21bf3bc0ff57e0b14f6b259eb3
Jesús Alejos <jalejos6@gmail.com>
2022-05-07 21:16:55 -0430

    completing challenge discounts coupons

--- descuentos.js
+++ descuentos.js
@@ -1,8 +1,8 @@
 // const precioOriginal = 120;
 // const descuento = 18;
 
-
 function calcularPrecioConDescuento(precio, descuento) {
+
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
@@ -13,13 +13,40 @@ function onClickButtonPriceDiscount() {
   const inputPrice = document.getElementById("InputPrice");
   const priceValue = inputPrice.value;
   
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = inputDiscount.value;
+  const inputCupon = document.getElementById("InputCupon");
+  const couponValue = inputCupon.value;
 
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  const coupons = [
+    {
+    name: "cupon mariano",
+    discount: 30,
+    },
+    {
+    name: "cupon de las madres",
+    discount: 20,
+    },
+    {
+    name: "cupon buen pastor",
+    discount: 10,
+    },
+  ];
 
+const isCouponValueValid = function (coupon) { //atention with the coupon.name
+    return coupon.name === couponValue;
+};
+
+const userCoupon = coupons.find(isCouponValueValid);
+
+if (!userCoupon) {
+  alert ("El cupón " + couponValue + " no es válido");
+} else {
+  let descuento = userCoupon.discount;
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
   const resultP = document.getElementById("ResultP");
-  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
+
+  
 }
 
 
