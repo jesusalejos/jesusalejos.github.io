4ea8c8640e54f386d8df55bd5a0193c5f178ae89
Jesús Alejos <jalejos6@gmail.com>
2022-05-09 07:49:59 -0430

    Completing Promedio Challenge for class is excelent"

--- promedio.js
+++ promedio.js
@@ -1,17 +1,30 @@
+function calcularMediaAritmetica() {
+  const inputLista = document.getElementById("Notas");
+  const valueLista = inputLista.value;
+  const resultado = valueLista.split(" ");
+  
 
-function calcularMediaAritmetica(lista) {
-  // let sumaLista = 0;
-  // for (let i = 0; i < lista.length; i++) {
-  //   sumaLista = sumaLista + lista[i];
-  // }
+  let sumaLista = 0;
+  for (let i = 0; i < resultado.length; i++) {
+    resultado[i] = parseInt(resultado[i])
+    sumaLista = sumaLista + resultado[i];
+    
+    console.log(resultado);
+    console.log(sumaLista);
+  }
 
-  const sumaLista = lista.reduce(
-    function (valorAcumulado = 0, nuevoElemento) {
-      return valorAcumulado + nuevoElemento;
-    }
-  );
+  // const sumaLista = resultado.reduce(
+  //    function (valorAcumulado = 0, nuevoElemento) {
+  //     return valorAcumulado + nuevoElemento;
+  //   }
+  // );
 
-  const promedioLista = sumaLista / lista.length;
+  const promedioLista = sumaLista / resultado.length;
 
-  return promedioLista;
+  promedioLista;
+
+  const resultP = document.getElementById("ResultP");
+
+  resultP.innerText = "El promedio de notas es " + promedioLista
+
 }
