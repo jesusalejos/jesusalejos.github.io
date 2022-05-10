eed3a1e2e782bb9edb1ed85c7a30b863de54fb2c
Jesús Alejos <jalejos6@gmail.com>
2022-05-10 06:53:35 -0430

    Challenge fo the moda ready

--- moda.js
+++ moda.js
@@ -1,20 +1,26 @@
-const lista1 = [
-  1,
-  2,
-  3,
-  1,
-  2,
-  3,
-  4,
-  2,
-  2,
-  2,
-  1,
-];
+// const lista1 = [
+//   1,
+//   2,
+//   3,
+//   1,
+//   2,
+//   3,
+//   4,
+//   2,
+//   2,
+//   2,
+//   1,
+// ];
+
+function calcularModa (){
+  const inputLista = document.getElementById("moda");
+  const valueLista = inputLista.value;
+  let resultado = valueLista.split(" ");
+
 
 const lista1Count = {};
 
-lista1.map(
+resultado.map(
   function (elemento) {
     if (lista1Count[elemento]) {
       lista1Count[elemento] += 1;
@@ -23,12 +29,20 @@ lista1.map(
     }
   }
 );
+//  console.log(resultado)
+//  console.log(lista1Count)
 
-const lista1Array = Object.entries(lista1Count).sort(
+ const lista1Array = Object.entries(lista1Count).sort(
   function (elementoA, elementoB) {
     return elementoA[1] - elementoB[1];
   }
-);
+);;
 
-const moda = lista1Array[lista1Array.length - 1];
+//  console.log(lista1Array);
 
+ const moda = lista1Array[lista1Array.length - 1];
+
+ const resultP = document.getElementById("ResultP");
+
+resultP.innerText = "La moda de los datos ingresados es " + moda[0];
+}
